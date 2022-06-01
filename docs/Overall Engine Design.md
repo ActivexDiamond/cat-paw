e53bbd7c19f28144a9eb2229c021f10212b2dc92
ActivexDiamond <activexdiamond@gmail.com>
2022-06-01 15:17:15 +0300

    Started building directory structure. Updated gitignore with love-packager dupe-conf.lua workaround.

---
+++ docs/Overall Engine Design.md
@@ -0,0 +1,6 @@
+# Title
+Hello, world!
+1. Why
+2. How
+3. goes
+	1. it
---
+++ src/main.lua
@@ -0,0 +1,28 @@
+--TODO: Fix how Love2D handles this.
+--io.stdout:setvbuf("line")
+
+
+
+-- This is the main entry point of your LOVE2D game.
+
+-- Feel free to delete the callbacks you don't need,
+-- and visit https://www.love2d.org/wiki/love if you need more.
+
+-- Load some default values for our rectangle.
+function love.load()
+	print 'x'
+	print 'y'
+	x, y, w, h = 20, 20, 60, 20
+end
+
+-- Increase the size of the rectangle every frame.
+function love.update(dt)
+	w = w + 1
+	h = h + 1
+end
+
+-- Draw a coloured rectangle.
+function love.draw()
+	love.graphics.setColor(0, 0.4, 0.4)
+	love.graphics.rectangle("fill", x, y, w, h)
+end
